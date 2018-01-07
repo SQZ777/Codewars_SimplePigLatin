d7896fc9655bbe6507d3be13a62559f1b9d2d0fa
sqz777 <sqz777isme@gmail.com>
2018-01-07 17:26:54 +0800

    加入專案檔案。

---
+++ Codewars_SimplePigLatin/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Codewars_SimplePigLatin
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
