cbda3c9d4254cd7a213eef30527f5a4dfabc7bc8
sqz777 <sqz777isme@gmail.com>
2018-01-07 18:12:40 +0800

    pass PigPart test case : input a

--- Codewars_SimplePigLatin/UnitTest1.cs
+++ Codewars_SimplePigLatin/UnitTest1.cs
@@ -23,6 +23,10 @@ namespace Codewars_SimplePigLatin
     {
         public static string PigPart(string s)
         {
+            if (s.Length > 0)
+            {
+                return s + "ay";
+            }
             return string.Empty;
         }
     }
